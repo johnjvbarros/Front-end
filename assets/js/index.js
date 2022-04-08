094b2dc4e3f2e1f5315b5b09937f33961ead50e4
JV <jv.jbarros@gmail.com>
2022-04-08 20:43:31 -0300

    Create apples website clone

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+(function() {const selectElement = (element) => document.querySelector(element);
+
+selectElement('.mobile-menu').addEventListener('click', () =>{
+    selectElement('header').classList.toggle('active');
+})}).call(this);
